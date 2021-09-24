1a8184f51fe60a156087a46f272bed70470ea9be
itsatifsiddiqui <itsatifsiddiqui@gmail.com>
2021-09-24 11:32:43 +0500

    launcher icon

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/atif/Downloads/flutter_versions/v2.2"
+export "FLUTTER_APPLICATION_PATH=/Users/atif/Desktop/Flutter World/conic"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=1"
+export "EXCLUDED_ARCHS=arm64"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
